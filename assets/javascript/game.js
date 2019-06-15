3f3c7810848ebaed6dcb838ba2e886c6b91e3728
CS <Cara@MBP15in2015.local>
2019-06-15 12:20:25 -0400

    value of crystal 1 successfully adds to score as a sum, but not sure if method used is the best method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,8 @@
 // We start the game with the following stats:
 var wins = 0;
 var losses = 0;
-var score = []
-var reducer = (score, crystal1)
+var score = [];
+
 
 // Function that randomly generates a number between 1 - 120
 var randomNumber = [Math.floor(Math.random() * 101 + 20)];
@@ -12,9 +12,8 @@ var randomNumber = [Math.floor(Math.random() * 101 + 20)];
     $("#random-number").text(randomNumber);
 
 // Crystal 1: function that randomly generates a number between 1 - 12
-var crystal1 = [Math.floor(Math.random() * 12 + 1)];
+var crystal1 = parseInt([Math.floor(Math.random() * 12 + 1)]);
     console.log("Crystal 1 = " + crystal1);
-    console.log(typeof(crystal1));
 
 // Crystal 2: function that randomly generates a number between 1 - 12
 var crystal2 = [Math.floor(Math.random() * 12 + 1)];
@@ -32,11 +31,10 @@ var crystal4 = [Math.floor(Math.random() * 12 + 1)];
 // When you click each crystal, their randomly assigned value will get added to the score box
 // N.B. Still need to figure out how to add the values together
 $("#crystal-1").on("click", function () {
-    score.push(parseInt(crystal1));
     $("#score").text(crystal1);
+    score = +score + +crystal1
     console.log(typeof(crystal1));
-    console.log(score);
-    // console.log(score.reduce(reducer));
+    console.log("Score: " + score);
 });
 
 $("#crystal-2").on("click", function() {
