dfa9dcf88f13dab65042276061df9ab72238afe4
CS <Cara@MBP15in2015.local>
2019-06-15 11:51:59 -0400

    Crystal1 value is pushed onto score array but the array doesn't yet reduce to a sum

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,8 @@
 // We start the game with the following stats:
 var wins = 0;
 var losses = 0;
-var score = [];
+var score = []
+var reducer = (score, crystal1)
 
 // Function that randomly generates a number between 1 - 120
 var randomNumber = [Math.floor(Math.random() * 101 + 20)];
@@ -13,6 +14,7 @@ var randomNumber = [Math.floor(Math.random() * 101 + 20)];
 // Crystal 1: function that randomly generates a number between 1 - 12
 var crystal1 = [Math.floor(Math.random() * 12 + 1)];
     console.log("Crystal 1 = " + crystal1);
+    console.log(typeof(crystal1));
 
 // Crystal 2: function that randomly generates a number between 1 - 12
 var crystal2 = [Math.floor(Math.random() * 12 + 1)];
@@ -30,7 +32,11 @@ var crystal4 = [Math.floor(Math.random() * 12 + 1)];
 // When you click each crystal, their randomly assigned value will get added to the score box
 // N.B. Still need to figure out how to add the values together
 $("#crystal-1").on("click", function () {
-    $("#score").text(score + crystal1)
+    score.push(parseInt(crystal1));
+    $("#score").text(crystal1);
+    console.log(typeof(crystal1));
+    console.log(score);
+    // console.log(score.reduce(reducer));
 });
 
 $("#crystal-2").on("click", function() {
