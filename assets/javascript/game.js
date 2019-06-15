61543a43d812b35c84c5181b8141aa9c988a3625
CS <Cara@MBP15in2015.local>
2019-06-15 11:07:21 -0400

    All random numbers are successfully generated

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+
+// We start the game with the following stats:
+var score = 0;
+var losses = 0;
+
+// Function that randomly generates a number between 1 - 120
+var randomNumber = [Math.floor(Math.random() * 101 + 20)];
+    console.log("Random number = " + randomNumber);
+    $("#random-number").text(randomNumber);
+
+// Crystal 1: function that randomly generates a number between 1 - 12
+var crystal1 = [Math.floor(Math.random() * 12 + 1)];
+    console.log("Crystal 1 = " + crystal1);
+
+// Crystal 2: function that randomly generates a number between 1 - 12
+var crystal2 = [Math.floor(Math.random() * 12 + 1)];
+    console.log("Crystal 2 = " + crystal2);
+
+// Crystal 3: function that randomly generates a number between 1 - 12
+var crystal3 = [Math.floor(Math.random() * 12 + 1)];
+    console.log("Crystal 3 = " + crystal3);
+
+// Crystal 4: function that randomly generates a number between 1 - 12
+var crystal4 = [Math.floor(Math.random() * 12 + 1)];
+    console.log("Crystal 4 = " + crystal4);
