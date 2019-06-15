f2b5e07b56691cc6fd4faded2bce6b42159fca20
CS <Cara@MBP15in2015.local>
2019-06-15 12:27:37 -0400

    Used parseInt to change type of score and all randomly generated numbers to number and no longer need weird + method to get score to add as numbers instead of strings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,13 @@
 // We start the game with the following stats:
 var wins = 0;
 var losses = 0;
-var score = [];
+var score = 0;
 
 
 // Function that randomly generates a number between 1 - 120
-var randomNumber = [Math.floor(Math.random() * 101 + 20)];
+var randomNumber = parseInt([Math.floor(Math.random() * 101 + 20)]);
     console.log("Random number = " + randomNumber);
+    console.log(typeof(randomNumber));
     $("#random-number").text(randomNumber);
 
 // Crystal 1: function that randomly generates a number between 1 - 12
@@ -16,15 +17,15 @@ var crystal1 = parseInt([Math.floor(Math.random() * 12 + 1)]);
     console.log("Crystal 1 = " + crystal1);
 
 // Crystal 2: function that randomly generates a number between 1 - 12
-var crystal2 = [Math.floor(Math.random() * 12 + 1)];
+var crystal2 = parseInt([Math.floor(Math.random() * 12 + 1)]);
     console.log("Crystal 2 = " + crystal2);
 
 // Crystal 3: function that randomly generates a number between 1 - 12
-var crystal3 = [Math.floor(Math.random() * 12 + 1)];
+var crystal3 = parseInt([Math.floor(Math.random() * 12 + 1)]);
     console.log("Crystal 3 = " + crystal3);
 
 // Crystal 4: function that randomly generates a number between 1 - 12
-var crystal4 = [Math.floor(Math.random() * 12 + 1)];
+var crystal4 = parseInt([Math.floor(Math.random() * 12 + 1)]);
     console.log("Crystal 4 = " + crystal4);
 
 
@@ -32,8 +33,7 @@ var crystal4 = [Math.floor(Math.random() * 12 + 1)];
 // N.B. Still need to figure out how to add the values together
 $("#crystal-1").on("click", function () {
     $("#score").text(crystal1);
-    score = +score + +crystal1
-    console.log(typeof(crystal1));
+    score = score + crystal1
     console.log("Score: " + score);
 });
 
